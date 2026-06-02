3d209eef24b05df0ecdf47ac9c054a8e0f4ebe3c
123lethabomosoathupa <lethabo.mosoathupa8@gmail.com>
2026-06-02 16:04:41 +0200

    Update

---
+++ chapter2/src/main/java/com/mycompany/chapter2/Dog.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chapter2;
+
+/**
+ *
+ * @author letha
+ */
+public class Dog {
+
+    int size;
+    String breed;
+    String name;
+
+    void bark() {
+        System.out.println("Ruff! Ruff!");
+    }
+}
---
+++ chapter2/src/main/java/com/mycompany/chapter2/DogTestDrive.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chapter2;
+
+/**
+ *
+ * @author letha
+ */
+public class DogTestDrive {
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
