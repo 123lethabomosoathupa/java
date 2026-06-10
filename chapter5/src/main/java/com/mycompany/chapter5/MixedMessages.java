b9770b248629f704b3e18dd00a94e1278922f1fb
123lethabomosoathupa <lethabo.mosoathupa8@gmail.com>
2026-06-10 14:18:41 +0200

    Update

---
+++ chapter5/src/main/java/com/mycompany/chapter5/MixedMessages.java
@@ -0,0 +1,19 @@
+package com.mycompany.chapter5;
+
+public class MixedMessages {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 30;
+        for (int outer = 0; outer < 3; outer++) {
+            for (int inner = 4; inner > 1; inner--) {
+                y = y - 2;
+                if (x == 6) {
+                    break;
+                }
+                // CANDIDATE CODE GOES HERE
+            }
+            y = y - 2;
+        }
+        System.out.println(x + " " + y);
+    }
+}
