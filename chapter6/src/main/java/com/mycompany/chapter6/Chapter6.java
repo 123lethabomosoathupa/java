88633923e37329ce14584497553e0599b86c20c5
123lethabomosoathupa <lethabo.mosoathupa8@gmail.com>
2026-06-10 14:23:32 +0200

    Update

---
+++ chapter6/src/main/java/com/mycompany/chapter6/Chapter6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chapter6;
+
+/**
+ *
+ * @author letha
+ */
+public class Chapter6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
