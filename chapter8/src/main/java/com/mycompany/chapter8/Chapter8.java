4d2e8954c33dcd76c500920b10bf8ea875c0ae95
123lethabomosoathupa <lethabo.mosoathupa8@gmail.com>
2026-06-11 22:18:45 +0200

    Update

---
+++ chapter8/src/main/java/com/mycompany/chapter8/Chapter8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chapter8;
+
+/**
+ *
+ * @author letha
+ */
+public class Chapter8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
